dad512edf69756d1f9e97cf497df4672e6e8cd94
Kapendev <alexandroskapretsos@gmail.com>
2026-05-04 13:26:13 +0300

    Fixed bit set naming + range.

--- examples/_011_bitset.d
+++ examples/_011_bitset.d
@@ -7,42 +7,55 @@ void main() {
     assert(bs.none == true);
     assert(bs.any == false);
     assert(bs.all == false);
-    assert(bs.count == 0);
+    assert(bs.length == 0);
 
     bs.set();
     assert(bs.none == false);
     assert(bs.any == true);
     assert(bs.all == true);
-    assert(bs.count == 64);
+    assert(bs.length == 64);
 
     bs.reset();
     assert(bs.none == true);
     assert(bs.any == false);
     assert(bs.all == false);
-    assert(bs.count == 0);
+    assert(bs.length == 0);
 
     bs[0] = true;
     assert(bs[0] == true);
-    assert(bs.count == 1);
+    assert(bs.length == 1);
 
     bs[3] = true;
     assert(bs[3] == true);
-    assert(bs.count == 2);
+    assert(bs.length == 2);
 
     bs[0] = false;
     assert(bs[0] == false);
-    assert(bs.count == 1);
+    assert(bs.length == 1);
 
     bs.flip(3);
     assert(bs[3] == false);
     assert(bs.none == true);
     bs.flip(7);
     assert(bs[7] == true);
-    assert(bs.count == 1);
+    assert(bs.length == 1);
 
     bs.reset();
     bs[1] = true;
     bs[5] = true;
     bs[62] = true;
-    assert(bs.count() == 3);
+    assert(bs.length == 3);
+
+    Sz[3] expected = [1, 5, 62];
+    Sz i = 0;
+    foreach (bit; bs.setBits) {
+        assert(bit == expected[i]);
+        i += 1;
+    }
+    assert(i == 3);
+
+    bs.reset();
+    Sz testCount = 0;
+    foreach (bit; bs.setBits) testCount += 1;
+    assert(testCount == 0);
 }
